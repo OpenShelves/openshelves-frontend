2b9d74dd035bdb9c859f86faf9bd803ca356c2df
Hannes Schulze <hannes.schulze@byte-solution.net>
2022-11-03 20:59:10 +0100

    Moved FIles

---
+++ android/app/src/main/kotlin/com/example/openshelves/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.openshelves
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- openshelves/android/app/src/main/kotlin/com/example/openshelves/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.openshelves
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
