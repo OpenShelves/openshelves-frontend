981bf2bd93f7793e12801a1118da42da10135664
Hannes Schulze <hannes.schulze@byte-solution.net>
2022-11-29 20:22:26 +0100

    Code Cleanup and finxing product store

--- lib/products/product_service.dart
+++ lib/products/product_service.dart
@@ -1,14 +1,15 @@
 import 'dart:convert';
 
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:http/http.dart' as http;
 import 'package:openshelves/constants.dart';
 import 'package:openshelves/products/product_model.dart';
 
 Future<Product> storeProduct(Product product) async {
+  var token = await getToken();
   final response = await http.post(Uri.parse(URL + '/product'),
       headers: <String, String>{
         'Content-Type': 'application/json; charset=UTF-8',
+        'Authorization': 'Bearer ' + token
       },
       body: jsonEncode(product));
 
@@ -31,6 +32,22 @@ Future<List<Product>> getProducts() async {
   if (response.statusCode == 200) {
     Iterable l = json.decode(response.body);
     return List<Product>.from(l.map((model) => Product.fromJson(model)));
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load product');
+  }
+}
+
+Future<Product> getProductById(int id) async {
+  var token = await getToken();
+  final response = await http.get(Uri.parse(URL + '/products'),
+      headers: <String, String>{
+        'Accept-Language': 'application/json',
+        'Authorization': 'Bearer ' + token
+      });
+  if (response.statusCode == 200) {
+    return Product.fromJson(jsonDecode(response.body));
   } else {
     // If the server did not return a 200 OK response,
     // then throw an exception.
